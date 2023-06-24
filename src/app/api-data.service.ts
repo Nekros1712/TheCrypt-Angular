ce2d0094acff9ee6c246b32bbeeaa5d97aafbd87
Nekros1712 <sampritchaurasiya07@gmail.com>
2023-06-24 16:34:44 +0530

    Add contact section

--- src/app/api-data.service.ts
+++ src/app/api-data.service.ts
@@ -7,16 +7,11 @@ import { Observable } from 'rxjs'
 })
 export class ApiDataService {
 
-  apiUrl: string = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en'
-  newsApiUrl: string = 'https://markets.sh/api/v1/clusters?api_token=60db70e00e439ed62fd525aeffa9a993'
+  apiUrl: string = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en'
 
   constructor(private http: HttpClient) { }
 
   getCoinsList(): Observable<any> {
     return this.http.get<any>(this.apiUrl)
-  }
-
-  getNews(): Observable<any> {
-    return this.http.get<any>(this.newsApiUrl)
   }
 }
