8b98ba0719003b50864307765ee94492fe28f329
Nekros1712 <sampritchaurasiya07@gmail.com>
2023-07-11 23:55:38 +0530

    Update backend api url

--- src/app/api-data.service.ts
+++ src/app/api-data.service.ts
@@ -6,7 +6,7 @@ import axios from 'axios'
 })
 export class ApiDataService {
 
-  backendApiUrl: string = 'http://localhost:3000/api/'
+  backendApiUrl: string = 'https://thecryptserver.cyclic.app/api/'
   coingeckoApiUrl: string = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en'
   dropdownData: any = {}
   
